35e031fb15e4cf2ed14456690d4125c18b6e314b
aletinte-creator <aletinte@gmail.com>
2026-05-20 19:17:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+from fastapi import FastAPI
+
 app = FastAPI()
+
 @app.get("/")
+def home():
+    return {"message": "PADE API funcionando"}
+
+@app.get("/ping")
+def ping():
+    return {"status": "ok"}
+``
